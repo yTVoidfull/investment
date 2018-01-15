159e7321727b50b050bbbf6a7a2edee50073d2e0
Alexandru Plesca <sandu_pleshca@hotmail.com>
2018-01-15 12:22:51 +0200

    initial commit - main activity layout

---
+++ app/src/main/java/com/example/alplesca/graweinvestmentcalculator/InvestmentCalculator.java
@@ -0,0 +1,22 @@
+package com.example.alplesca.graweinvestmentcalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class InvestmentCalculator extends AppCompatActivity {
+
+    EditText age;
+    EditText amount;
+    EditText period;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_investment_calculator);
+
+        age = findViewById(R.id.ageInput);
+        amount = findViewById(R.id.amountInput);
+        period = findViewById(R.id.periodInput);
+    }
+}
